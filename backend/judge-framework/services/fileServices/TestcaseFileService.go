5515a3eb225b4919656d3d2a4b097f5cb5b62c7b
Md Moniruzzaman <skmnrcse@gmail.com>
2023-04-28 18:49:40 +0700

    fix issue with output file

--- backend/judge-framework/services/fileServices/TestcaseFileService.go
+++ backend/judge-framework/services/fileServices/TestcaseFileService.go
@@ -38,7 +38,7 @@ func SaveTestcasesIntoFiles(platform string, cid string, label string, testcases
 	inputDirectory, outputDirectory := GetInputOutputDirectories(platform, cid, label)
 	for id, testcase := range testcases {
 		inputFilename := fmt.Sprintf("00_sample_input_%03d.txt", id+1)
-		outputFilename := fmt.Sprintf("00_sample_output_%03d.txt", id)
+		outputFilename := fmt.Sprintf("00_sample_output_%03d.txt", id+1)
 		go utils.WriteFileContent(inputDirectory, inputFilename, []byte(testcase.Input))
 		go utils.WriteFileContent(outputDirectory, outputFilename, []byte(testcase.Output))
 	}
